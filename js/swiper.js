26bd3306086fdb551bb556b54924f43a2926b8ae
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2022-10-06 17:57:04 +0300

    update

---
+++ js/swiper.js
@@ -0,0 +1,30 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    //   type: 'progressbar',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    disabledClass: 'swiper-button-disabled',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+
+  mousewheel: {
+    invert: false,
+    releaseOnEdges: true,
+    eventsTarget: '.section__about',
+  },
+});
