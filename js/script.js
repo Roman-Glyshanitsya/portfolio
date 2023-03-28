dea8fed00763f5693a31a25458a0e82a9860a5ee
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2023-03-29 02:11:28 +0300

    update

---
+++ js/script.js
@@ -0,0 +1,51 @@
+'use strict';
+
+function imagesInit() {
+  const images = document.querySelectorAll('.work__image');
+  if (images.lenght) {
+    images.forEach(image => {
+      const imageItem = image.querySelector('img');
+      const padding = (imageItem.offsetHeight / imageItem.offsetWidth) * 100;
+      image.style.paddingBottom = `${padding}%`;
+      imageItem.classList.add('init');
+    });
+  }
+}
+
+function gridInit() {
+  const items = document.querySelector('.works__items');
+  const itemsGrid = new Isotope(items, {
+    itemSelector: '.work',
+    masonry: {
+      fitWidth: true,
+      gutter: 20,
+    },
+  });
+
+  document.addEventListener('click', documentActions);
+
+  function documentActions(e) {
+    const targetElement = e.target;
+    if (targetElement.closest('.filter-works__item')) {
+      const filterItem = targetElement.closest('.filter-works__item');
+      const filterValue = filterItem.dataset.filter;
+      const filterActiveItem = document.querySelector('.filter-works__item.active');
+
+      filterValue === '*'
+        ? itemsGrid.arrange({ filter: `` })
+        : itemsGrid.arrange({ filter: `[data-filter="${filterValue}"]` });
+
+      filterActiveItem.classList.remove('active');
+      filterItem.classList.add('active');
+
+      e.preventDefault();
+    }
+  }
+}
+
+window.addEventListener('load', windowLoad);
+
+function windowLoad() {
+  imagesInit();
+  gridInit();
+}
