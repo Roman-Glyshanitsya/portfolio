d1d20e4637d61aeedf5951c766731392f4f1b875
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2023-09-08 02:47:08 +0300

    update

--- js/dark-theme.js
+++ js/dark-theme.js
@@ -1,5 +1,12 @@
 const icon = document.querySelector('[dark-theme]');
+const text = document.querySelector('.dark__theme__text');
 
 icon.onclick = function () {
   document.body.classList.toggle('dark__theme__mode');
+
+  if (document.body.classList.contains('dark__theme__mode')) {
+    text.textContent = 'Switch light';
+  } else {
+    text.textContent = 'Switch dark';
+  }
 };
