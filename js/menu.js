73026b57ac4b4e821d3a6ca7b7fb2344455ac1ab
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2023-09-08 20:33:44 +0300

    update

--- js/menu.js
+++ js/menu.js
@@ -11,3 +11,41 @@
     mobileMenuRef.classList.toggle('is-open');
   });
 })();
+
+// Icon rotate
+const svgElement = document.getElementById('mySvg');
+
+svgElement.addEventListener('click', () => {
+  svgElement.classList.toggle('active');
+});
+
+// Anchor
+const mobileMenuRef = document.querySelector('[data-menu]');
+
+const menuLinks = document.querySelectorAll('.nav__link[data-goto]');
+if (menuLinks.length > 0) {
+  menuLinks.forEach(menuLink => {
+    menuLink.addEventListener('click', onMenuLink);
+  });
+
+  function onMenuLink(e) {
+    const menuLink = e.target;
+    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue =
+        gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
+
+      if (mobileMenuRef.classList.contains('is-open')) {
+        mobileMenuRef.classList.remove('is-open');
+        svgElement.classList.remove('active');
+      }
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: 'smooth',
+      });
+
+      e.preventDefault();
+    }
+  }
+}
